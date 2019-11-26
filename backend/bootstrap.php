dfcb32dfff38092d63c2efdc05baacde0aa90ef2
Bruno Fernandes Campos <bruno.campos@teknisa.com>
2019-11-26 16:11:09 -0300

    bootstrap

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
 
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-
 require_once "vendor/autoload.php";
-
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/Entity"), true);
