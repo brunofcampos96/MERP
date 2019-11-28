9f6a86eb8d9e9452e6fea7c6e4dc22d18681ef00
Bruno Fernandes Campos <bruno.campos@teknisa.com>
2019-11-28 18:37:40 -0300

    almost2

--- backend/default.sql
+++ backend/default.sql
@@ -2,3 +2,11 @@ INSERT INTO specialty(`description`) VALUES('Pedriatria');
 INSERT INTO specialty(`description`) VALUES('Ortopedia');
 INSERT INTO specialty(`description`) VALUES('Dermatologia');
 INSERT INTO specialty(`description`) VALUES('Oftalmologia');
+
+INSERT INTO `user`(`email`, `name`, `password`) VALUES ('bruno@teste.com','Bruno Fernandes Campos','698dc19d489c4e4db73e28a713eab07b')
+
+
+
+vendor/bin/doctrine orm:schema-tool:create
+
+vendor/bin/doctrine orm:schema-tool:drop --force
